66969d3d4873882102146f6eee39d7c455787b48
genesis.aquino <genesis.aquino@americanas.io>
2023-08-08 20:40:59 -0300

    implementation navigation

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         installSplashScreen()
