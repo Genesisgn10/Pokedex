3cb69e3e90d1af57cfba533eed79bbdac016ca76
Ryudi07 <rafael_yudi07@hotmail.com>
2023-08-09 12:06:40 -0400

    subindo a implementacao da biblioteca Coil de imagens

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import coil.load
+import coil.transform.CircleCropTransformation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.load("https://th.bing.com/th/id/OIP.yAwZHsnVyP9dvAKnseexRgHaGp?w=233&h=209&c=7&r=0&o=5&pid=1.7"){
+            crossfade(true)
+            placeholder(R.drawable.ic_launcher_foreground)
+            transformations(CircleCropTransformation())
+        }
     }
 }
