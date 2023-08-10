994d59ea76edfba01ec3c76272d45a0936053544
Hélio Júnior <heliojunioferreira10@gmail.com>
2023-08-10 18:32:13 -0300

    add RecyclerView list.

---
+++ app/src/main/java/com/example/pokedex/AdapterGridList.kt
@@ -0,0 +1,38 @@
+package com.example.pokedex
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterGridList(private val names: List<String>) :
+    RecyclerView.Adapter<ViewHolderGridList>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderGridList {
+
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.grid_layout_list_item, parent, false)
+
+        return ViewHolderGridList(view)
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderGridList, position: Int) {
+        val item = names[position]
+        holder.bind(item)
+    }
+}
+
+class ViewHolderGridList(view: View) : RecyclerView.ViewHolder(view) {
+
+    val tvnumber = view.findViewById<TextView>(R.id.tv_numberpokemon)
+    val tvname = view.findViewById<TextView>(R.id.tv_namepokemon)
+
+    fun bind(name: String) {
+        tvname.text = name
+    }
+
+}
--- app/src/main/java/com/example/pokedex/PokedexFragment.kt
+++ app/src/main/java/com/example/pokedex/PokedexFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.pokedex.databinding.ActivityMainBinding
+import com.example.pokedex.databinding.FragmentPokedexBinding
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -20,18 +25,30 @@ class PokedexFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var binding: FragmentPokedexBinding?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
+        val list = listOf<String>("Bulbassauro 1", "Bulbassauro 2", "Bulbassauro 3")
+        val adapter = AdapterGridList(list)
+
+        binding?.rvPokemonlist?.adapter = adapter
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        binding = FragmentPokedexBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokedex, container, false)
+        return binding?.root
+
     }
 
     companion object {
