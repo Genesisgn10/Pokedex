35b81d266a2f2c9b99ef09f245ada2e3ba1b6cce
genesis.aquino <genesis.aquino@americanas.io>
2023-08-14 13:09:26 -0300

    Add toolbar project

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -8,12 +8,26 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //splash screen transition.
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar.myToolbar)
         setContentView(binding.root)
+
+        binding.toolbar.floatingActionButton.setOnClickListener {
+            showDialog()
+        }
     }
+
+    private fun showDialog() {
+        val fragmentManager = supportFragmentManager
+        val newFragment = Dialog()
+
+        newFragment.show(fragmentManager, "dialog")
+    }
+
 }
